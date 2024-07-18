354ef4a4a659ac5ce01217fc89008b755999e7cd
Ming <minhlaminh01@gmail.com>
2024-07-18 23:57:31 +0700

    First Design, Handle Logic

---
+++ WindowGUI/View/IPetView.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowGUI.View
+{
+    public interface IPetView
+    {
+        // Properties - Fields
+        string PetID { get; set; }
+        string PetName { get; set; }
+        string PetType { get; set; }
+        string PetColor { get; set; }
+
+        string SearchValue { get; set; }
+        bool IsEdit { get; set; }
+        bool IsSuccessful { get; set; }
+        string Message { get; set; }
+
+        // Events
+        event EventHandler SearchEvent;
+        event EventHandler AddNewEvent;
+        event EventHandler EditEvent;
+        event EventHandler DeleteEvent;
+        event EventHandler SaveEvent;
+        event EventHandler CancleEvent;
+
+        // Methods
+        void SetPetListBindingSource(BindingSource petList);
+        void Show();
+    }
+}
---
+++ WindowGUI/View/PetView.Designer.cs
@@ -0,0 +1,323 @@
+﻿namespace WindowGUI.View
+{
+    partial class PetView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabPagePet = new System.Windows.Forms.TabControl();
+            this.tabPagePetList = new System.Windows.Forms.TabPage();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tabPagePetDetail = new System.Windows.Forms.TabPage();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.txtPetColor = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtPetType = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtPetName = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtPetId = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.tabPagePet.SuspendLayout();
+            this.tabPagePetList.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.tabPagePetDetail.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 64);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(33, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "PETS";
+            // 
+            // tabPagePet
+            // 
+            this.tabPagePet.Controls.Add(this.tabPagePetList);
+            this.tabPagePet.Controls.Add(this.tabPagePetDetail);
+            this.tabPagePet.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabPagePet.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tabPagePet.Location = new System.Drawing.Point(0, 64);
+            this.tabPagePet.Name = "tabPagePet";
+            this.tabPagePet.SelectedIndex = 0;
+            this.tabPagePet.Size = new System.Drawing.Size(800, 386);
+            this.tabPagePet.TabIndex = 1;
+            // 
+            // tabPagePetList
+            // 
+            this.tabPagePetList.Controls.Add(this.dataGridView);
+            this.tabPagePetList.Controls.Add(this.btnDelete);
+            this.tabPagePetList.Controls.Add(this.btnEdit);
+            this.tabPagePetList.Controls.Add(this.btnAdd);
+            this.tabPagePetList.Controls.Add(this.btnSearch);
+            this.tabPagePetList.Controls.Add(this.txtSearch);
+            this.tabPagePetList.Controls.Add(this.label2);
+            this.tabPagePetList.Location = new System.Drawing.Point(4, 29);
+            this.tabPagePetList.Name = "tabPagePetList";
+            this.tabPagePetList.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPagePetList.Size = new System.Drawing.Size(792, 353);
+            this.tabPagePetList.TabIndex = 0;
+            this.tabPagePetList.Text = "Pet list";
+            this.tabPagePetList.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(34, 87);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.RowHeadersWidth = 51;
+            this.dataGridView.RowTemplate.Height = 24;
+            this.dataGridView.Size = new System.Drawing.Size(624, 242);
+            this.dataGridView.TabIndex = 7;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(682, 172);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(85, 35);
+            this.btnDelete.TabIndex = 6;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(682, 129);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(85, 35);
+            this.btnEdit.TabIndex = 5;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(682, 87);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(85, 35);
+            this.btnAdd.TabIndex = 4;
+            this.btnAdd.Text = "Add New";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(583, 43);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 28);
+            this.btnSearch.TabIndex = 2;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(34, 45);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(528, 26);
+            this.txtSearch.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 14);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Search Pet:";
+            // 
+            // tabPagePetDetail
+            // 
+            this.tabPagePetDetail.Controls.Add(this.button6);
+            this.tabPagePetDetail.Controls.Add(this.button5);
+            this.tabPagePetDetail.Controls.Add(this.txtPetColor);
+            this.tabPagePetDetail.Controls.Add(this.label6);
+            this.tabPagePetDetail.Controls.Add(this.txtPetType);
+            this.tabPagePetDetail.Controls.Add(this.label5);
+            this.tabPagePetDetail.Controls.Add(this.txtPetName);
+            this.tabPagePetDetail.Controls.Add(this.label4);
+            this.tabPagePetDetail.Controls.Add(this.txtPetId);
+            this.tabPagePetDetail.Controls.Add(this.label3);
+            this.tabPagePetDetail.Location = new System.Drawing.Point(4, 29);
+            this.tabPagePetDetail.Name = "tabPagePetDetail";
+            this.tabPagePetDetail.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPagePetDetail.Size = new System.Drawing.Size(792, 353);
+            this.tabPagePetDetail.TabIndex = 1;
+            this.tabPagePetDetail.Text = "Pet detail";
+            this.tabPagePetDetail.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(243, 292);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(180, 36);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "Cancel";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(34, 292);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(180, 36);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "Save";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // txtPetColor
+            // 
+            this.txtPetColor.Location = new System.Drawing.Point(34, 240);
+            this.txtPetColor.Name = "txtPetColor";
+            this.txtPetColor.Size = new System.Drawing.Size(387, 26);
+            this.txtPetColor.TabIndex = 7;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(30, 217);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(84, 20);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Pet Color:";
+            // 
+            // txtPetType
+            // 
+            this.txtPetType.Location = new System.Drawing.Point(261, 148);
+            this.txtPetType.Name = "txtPetType";
+            this.txtPetType.Size = new System.Drawing.Size(160, 26);
+            this.txtPetType.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(257, 125);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(80, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Pet Type:";
+            // 
+            // txtPetName
+            // 
+            this.txtPetName.Location = new System.Drawing.Point(34, 150);
+            this.txtPetName.Name = "txtPetName";
+            this.txtPetName.Size = new System.Drawing.Size(160, 26);
+            this.txtPetName.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(30, 125);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(88, 20);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Pet Name:";
+            // 
+            // txtPetId
+            // 
+            this.txtPetId.Location = new System.Drawing.Point(34, 60);
+            this.txtPetId.Name = "txtPetId";
+            this.txtPetId.Size = new System.Drawing.Size(160, 26);
+            this.txtPetId.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(30, 36);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 20);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Pet ID:";
+            // 
+            // PetView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabPagePet);
+            this.Controls.Add(this.panel1);
+            this.Name = "PetView";
+            this.Text = "PetView";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.tabPagePet.ResumeLayout(false);
+            this.tabPagePetList.ResumeLayout(false);
+            this.tabPagePetList.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.tabPagePetDetail.ResumeLayout(false);
+            this.tabPagePetDetail.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TabControl tabPagePet;
+        private System.Windows.Forms.TabPage tabPagePetList;
+        private System.Windows.Forms.TabPage tabPagePetDetail;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox txtPetId;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtPetType;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtPetName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtPetColor;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.DataGridView dataGridView;
+    }
+}
